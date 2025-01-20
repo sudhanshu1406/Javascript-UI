78bce281c98f2b8b24cd02017106a0cf5a1bf6dd
sudhanshu1406 <sudhanshumehto693@gmail.com>
2025-01-20 13:22:27 +0530

    complete

--- script.js
+++ script.js
@@ -25,6 +25,11 @@ function insert_data() {
         return: document.querySelector('.return1').value,
         traveling_as: document.querySelector('#traveling_as').value,
         payment: document.querySelector('#payment').value
+        // name: document.querySelector('#name').value,
+        // lastname: document.querySelector('#lastname').value,
+        // dateofbirth: document.querySelector('#dob').value,
+        // phone: document.querySelector('#phone').value,
+        // email: document.querySelector('#email').value
     }
 
     fetch("http://localhost:3000/data", {
@@ -74,4 +79,30 @@ function s(a){
     selectinput.style.outlineColor = "blace";
 
     return false;
+}
+// ==========================================================================================================================
+function singup(){
+    var data = {
+        username : document.querySelector("#name").value,
+        lastname : document.querySelector("#lastname").value,
+        password : document.querySelector("#password").value,
+    }
+
+    localStorage.setItem("singupdata",JSON.stringify(data));
+}
+
+var local_data = JSON.parse(localStorage.getItem("singupdata"));
+
+function login(){
+    let data = {
+        namelogin : document.getElementById("name").value,
+        lastnamelogin : document.getElementById("lastname").value,
+        passwordlogin : document.getElementById("password").value
+    }
+
+    if(local_data.name!=data.namelogin || local_data.password!=data.passwordlogin)
+    {
+        alert("user not found")
+        return false;
+    }
 }
